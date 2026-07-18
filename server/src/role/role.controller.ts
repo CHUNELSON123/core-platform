0c2ddbae71664ad8622e9ec0540e3688dedcfa5e
Ka-rol7 <karol@panus.cm>
2026-07-18 14:18:17 +0100

    feat(role): complete role CRUD module

--- server/src/role/role.controller.ts
+++ server/src/role/role.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { RoleService } from './role.service';
+import { UpdateRoleDto } from './dto/update-role.dto';  
 
 @Controller('roles')
 @UseGuards(JwtAuthGuard)
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
+
+   @Get()
+    findAll(@Req() request) {
+      return this.roleService.findAll(request.user.organizationId);
+    }
+
+  @Get(':id')
+  findOne(@Req() request, @Param('id') roleId: string) {
+    return this.roleService.findOne(request.user.organizationId, roleId);
+  }
 
   @Post()
   create(
@@ -15,4 +26,20 @@ export class RoleController {
   ) {
     return this.roleService.create(request.user.organizationId, createRoleDto);
   }
+
+  @Patch(':id')
+  update(
+    @Req() request,
+    @Param('id') roleId: string,
+    @Body() updateRoleDto: UpdateRoleDto
+  ) {
+    return this.roleService.update(request.user.organizationId, roleId, updateRoleDto);
+  }
+
+  @Delete(':id')
+  remove(@Req() request, @Param('id') roleId: string) {
+    return this.roleService.remove(request.user.organizationId, roleId);
+  }
+
+ 
 }
--- server/src/role/role.service.ts
+++ server/src/role/role.service.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class RoleService {
@@ -36,4 +37,117 @@ export class RoleService {
         data: role,
     };
  }
+
+ async findAll(organizationId: string) {
+    const roles = await this.prismaService.role.findMany({
+        where: {
+            organizationId,
+            deletedAt: null,
+        },
+        orderBy: {
+            name: 'asc',
+        },
+    });
+
+    return {
+        message: 'Roles retrieved successfully',
+        data: roles,
+    };
+  }  
+
+  async findOne(
+    organizationId: string,
+    roleId: string
+  ) {
+    const role = await this.prismaService.role.findFirst({
+      where: {
+         id: roleId,
+        organizationId,
+        deletedAt: null
+        },
+    });
+    if (!role) {
+      throw new NotFoundException('Role not found.');
+    }
+
+    return {
+      message: 'Role retrieved successfully',
+      data: role,
+    };
+  }
+
+  async update(
+    organizationId: string,
+    roleId: string,
+    updateRoleDto: UpdateRoleDto,
+  ) {
+    const role = await this.prismaService.role.findFirst({
+      where: {
+        id: roleId,
+        organizationId,
+        deletedAt: null
+      },
+    });
+
+    if (!role) {
+      throw new NotFoundException('Role not found.');
+    }
+
+    if (updateRoleDto.name && updateRoleDto.name !== role.name) {
+      const existingRole = await this.prismaService.role.findUnique({
+        where: {
+          organizationId_name: {
+            organizationId,
+            name: updateRoleDto.name,
+          },
+        },
+      });
+
+      if (existingRole) {
+        throw new BadRequestException('Role with this name already exists in the organization.');
+      }
+    }
+
+    const updatedRole = await this.prismaService.role.update({
+      where: {
+        id: roleId,
+      },
+      data: updateRoleDto,
+    });
+
+    return {
+      message: 'Role updated successfully',
+      data: updatedRole,
+    };
+  }
+
+  async remove(
+    organizationId: string,
+    roleId: string
+  ) {
+    const role = await this.prismaService.role.findFirst({
+      where: {
+        id: roleId,
+        organizationId,
+        deletedAt: null
+      },
+    });
+
+    if (!role) {
+      throw new NotFoundException('Role not found.');
+    }
+
+    await this.prismaService.role.update({
+      where: {
+        id: roleId,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+
+    return {
+      message: 'Role deleted successfully',
+    };
+  }
 }
